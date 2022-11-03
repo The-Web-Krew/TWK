90fc4bfedbce571841c9f9d406b25406f1f6c641
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2022-11-03 21:47:26 +0530

    Border correction

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
+import React from "react";
 // import React, { useState, useEffect, useRef } from 'react'
 import "../assets/css/aboutPage.css";
 import TWK from "../assets/Images/TWK5.png";
 
 // import Halo from 'vanta/dist/vanta.waves.min.js'
 
-
 function About() {
-
   // const [vantaEffect, setVantaEffect] = useState(null)
-  
+
   // const myRef = useRef(null)
   // useEffect(() => {
   //   if (!vantaEffect) {
@@ -35,10 +33,9 @@ function About() {
   //   }
   // }, [vantaEffect])
 
-
   return (
-    <div >
-    {/* <div className="aboutVanta" /*ref={myRef}/> */}
+    <div>
+      {/* <div className="aboutVanta" /*ref={myRef}/> */}
       <div className="contactHead">
         <h1 className="  aboutLetsTalkText gradientText">
           "Dignity does not consist in possessing honors, but in the
@@ -46,17 +43,62 @@ function About() {
         </h1>
       </div>
       {/* </div> */}
+     
+
+      <section className="developmentProcess  developerSection">
+        <div className="developmentProcessImage">
+          <img src={TWK} alt="/" />
+        </div>
+        <div className="developmentProcessDetails">
+          <h1>Our Aim</h1>
+          <h3 className="aim-vision-h3">
+            To consistently deliver our clients with profits, quality, content
+            that brings them the proper attention, and the best services
+            available is our fundamental aim to give us a strong purpose for
+            work..
+          </h3>
+        </div>
+      </section>
+
+      <section className="developmentProcess  developerSection">
+        <div className="developmentProcessDetails">
+          <h1>Our Vision </h1>
+          <h3 className="aim-vision-h3" >
+            A future with every business successfully digitized on the internet
+            is our vision and we are working hard towards that goal every day to
+            turn it into a reality.
+          </h3>
+        </div>
+        <div className="developmentProcessImage">
+          <img src={TWK} alt="/" />
+        </div>
+      </section>
+
+      {/* <section className="developmentProcess developerSection">
+        <div className="developmentProcessImage">
+          <img src={TWK} alt="/" />
+        </div>
+        <div className="developmentProcessDetails">
+          <h1>Development process</h1>
+          <h3>
+            TWK team is composed of Graphic Designers and Full Stack developers.
+            Our creative Team members take the highest level of pride in their
+            work.
+          </h3>
+        </div>
+      </section> */}
+
       <div className="developmentProcess">
         <div className="developmentProcessImage">
           <img src={TWK} alt="/" />
         </div>
         <div className="developmentProcessDetails">
           <h1>Development process</h1>
-          <h5>
+          <h4 className="aim-vision-h3">
             TWK team is composed of Graphic Designers and Full Stack developers.
             Our creative Team members take the highest level of pride in their
             work.
-          </h5>
+          </h4>
 
           <ul>
             <li>
@@ -72,50 +114,6 @@ function About() {
         </div>
       </div>
 
-
-      <section className="developmentProcess  developerSection">
-        <div className="developmentProcessImage">
-          <img src={TWK} alt="/" />
-        </div>
-        <div className="developmentProcessDetails">
-          <h1>Development process</h1>
-          <h5>
-            TWK team is composed of Graphic Designers and Full Stack developers.
-            Our creative Team members take the highest level of pride in their
-            work.
-          </h5>
-        </div>
-      </section>
-
-      <section className="developmentProcess  developerSection">
-        <div className="developmentProcessDetails">
-          <h1>Development process</h1>
-          <h5>
-            TWK team is composed of Graphic Designers and Full Stack developers.
-            Our creative Team members take the highest level of pride in their
-            work.
-          </h5>
-        </div>
-        <div className="developmentProcessImage">
-          <img src={TWK} alt="/" />
-        </div>
-      </section>
-
-      <section className="developmentProcess developerSection">
-        <div className="developmentProcessImage">
-          <img src={TWK} alt="/" />
-        </div>
-        <div className="developmentProcessDetails">
-          <h1>Development process</h1>
-          <h5>
-            TWK team is composed of Graphic Designers and Full Stack developers.
-            Our creative Team members take the highest level of pride in their
-            work.
-          </h5>
-        </div>
-      </section>
-
-      
     </div>
   );
 }
